3c5deff07360dc6afa1d3aa4b0a9a671810c43d6
alanzhang <hj_chang@163.com>
2017-06-20 10:51:43 +0800

    Initial commit

---
+++ src/cli/core.clj
@@ -0,0 +1,28 @@
+(ns cli.core
+  (:refer-clojure :exclude [parse-opts])
+  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
+
+(def cli-options
+  ;; An option with a required argument
+  [["-p" "--port PORT" "Port number"
+    :default 80
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ;; A non-idempotent option
+   ["-v" nil "Verbosity level"
+    :id :verbosity
+    :default 0
+    :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ;; A boolean option defaulting to nil
+   ["-h" "--help"]
+   ["-s" "--sql" "sql p1" "sql str"
+    :default ""]])
+
+
+(defn -main [& args]
+  (println (.toString (parse-opts args cli-options)))
+  ;(parse-opts args cli-options)
+  )
+
+(println "Cleanlines is next to godliness")
